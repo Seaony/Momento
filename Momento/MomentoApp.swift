6f380fc3bc0db703516262743b2eddb5d4cad8c2
Seaony <seaony@outlook.com>
2026-05-21 09:04:30 +0800

    Initial Commit

---
+++ Momento/MomentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentoApp.swift
+//  Momento
+//
+//  Created by Seaony on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MomentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
