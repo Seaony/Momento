f328a570cdd0489d071c63b3b0f6aa0fba213fbd
Seaony <seaony@outlook.com>
2026-05-22 22:55:17 +0800

    fix: lower main window minimum width

--- Momento/DesignSystem/MomentoGlass.swift
+++ Momento/DesignSystem/MomentoGlass.swift
@@ -74,7 +74,7 @@ enum MomentoTheme {
     static let contentMinWidth: CGFloat = 520
     static let contentSidebarGap: CGFloat = 12
     static let toolbarHeight: CGFloat = 56
-    static let mainWindowMinWidth = sidebarMaxWidth + floatingSidebarInset * 2 + contentMinWidth + contentSidebarGap * 2 + inspectorWidth
+    static let mainWindowMinWidth: CGFloat = 800
     static let mainWindowMinHeight: CGFloat = 640
     static let defaultWindowWidth: CGFloat = 1280
     static let defaultWindowHeight: CGFloat = 800
